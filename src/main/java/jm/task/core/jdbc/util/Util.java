5c4a536e4c084e0e947d8c8164ba60caeab05e5a
Eugene1917 <Bloody.30@mail.ru>
2022-07-15 14:33:22 +0400

    fixup! pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "12345");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
 }
